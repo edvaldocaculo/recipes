82e35a3a3dc38aa97645f06b0636262fbfcea2af
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2022-12-26 23:06:21 -0300

    atualizado

---
+++ recipes/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/pages/home.html', context={
+        'name':''
+    })

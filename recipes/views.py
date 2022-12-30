ab1186c7f57c3bcabada38e9d52f51aa81df7d7c
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2022-12-30 20:26:20 -0300

    views

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'name': ''})
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True
+        ).order_by('-id')
+    )
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - -Category| '
+    })
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(
+        Recipe.objects.filter(
+            pk=id,
+            is_published=True
+        )
+    )
     return render(request, 'recipes/pages/recipe-views.html', context={
-        'name': ''
+        'recipe': recipe,
+        'is_details_page': True,
     })

64d7b562780298ac84bfc80a796fd46e519fa8a0
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2022-12-30 20:25:01 -0300

    url_recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name='recipes-home'),
+    path('recipes/category/<int:category_id>/',
+         views.category, name='category'),
+    path('recipes/<int:id>/', views.recipe, name='recipe-details'),
 ]

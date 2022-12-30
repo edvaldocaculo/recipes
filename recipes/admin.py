a6a9f1dd512d43c0dfc10b425792ac452ba07b97
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2022-12-30 20:24:27 -0300

    admin

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+# Primeira forma de registrar na zona administrativa
+admin.site.register(models.Category, CategoryAdmin)
+
+# segunda forma registrar na zna administrativa
+
+
+@admin.register(models.Recipe)
+class RecipesAdmin(admin.ModelAdmin):
+    ...

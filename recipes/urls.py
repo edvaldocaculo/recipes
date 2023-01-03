015860421e010982dbd05bcc0f3c497062d3f099
Edvaldo Caculo <edvaldocaculo2@gmail.com>
2023-01-02 23:18:23 -0300

    urls recipes

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='recipes-home'),
+    path('recipes/search/', views.search, name='search'),
     path('recipes/category/<int:category_id>/',
          views.category, name='category'),
     path('recipes/<int:id>/', views.recipe, name='recipe-details'),
+
 ]
